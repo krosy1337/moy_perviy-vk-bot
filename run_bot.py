8ba5623afaeae3405a558c4a57ade4e6469d1f69
krosy1337 <krosy1337@gmail.com>
2018-12-08 22:57:14 +0300

    ok

--- run_bot.py
+++ run_bot.py
@@ -13,6 +13,17 @@ def send_photo(user_id, photo):
     vk_bot.method('messages.send', {'user_id': user_id, 'attachment': photo, 'random_id': random.randint(0, 1000)})
 
 
+def get_last_post(owner_id, count, offset, filter):
+    response = vk_bot_user.method('wall.get',
+                                  {'owner_id': owner_id, 'count': count, 'offset': offset, 'filter': filter})
+    return response['items'][0]['id']
+
+
+def write_msg_attach(user_id, text, attach):
+    vk_bot.method('messages.send', {'user_id': user_id, 'attachment': attach, 'message': text, 'random_id': random.randint(0, 1000)})
+
+
+
 PHOTO_2 = 'photo324207419_456239051'
 
 
@@ -22,7 +33,7 @@ def frwd_msg(user_id, messag, text):
 
 
 vk_bot = vk_api.VkApi(token=TOKEN)
-ACCESS_KEY = ACCESS_TOKEN
+vk_bot_user = vk_api.VkApi(token=ACCESS_TOKEN)
 long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
 server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
 print("готов к работе")
@@ -32,13 +43,13 @@ print("готов к работе")
 while True:
     long_poll = requests.get(
         'https://{server}?act={act}&key={key}&ts={ts}&wait=9999999999999999'.format(server=server,
-                                                                       act='a_check',
-                                                                       key=key,
-                                                                       ts=ts)).json()
+                                                                                    act='a_check',
+                                                                                    key=key,
+                                                                                    ts=ts)).json()
     update = long_poll['updates']
     if update[0][0] == 4:
         upd = update[0][6].lower()
-        if "прив" in upd:    # Если нам пишут привет
+        if "прив" in upd:  # Если нам пишут привет
             print(upd)
             user_id = update[0][3]
             user_name = vk_bot.method('users.get', {'user_ids': user_id})
@@ -56,7 +67,8 @@ while True:
                 user_id = update[0][3]
                 user_name = vk_bot.method('users.get', {'user_ids': user_id})
                 write_msg(user_id, 'Отлично, ' + (user_name[0]['first_name']))
-                print(str(user_name[0]['first_name']) + ' ' + str(user_name[0]['last_name']) + ' написал(а) боту - ' + str(
+                print(str(user_name[0]['first_name']) + ' ' + str(
+                    user_name[0]['last_name']) + ' написал(а) боту - ' + str(
                     update[0][6]))
         elif "пока" in upd:
             user_id = update[0][3]
@@ -70,6 +82,13 @@ while True:
             user_id = update[0][3]
             print(update)
             frwd_msg(user_id, update[0][1] - 2, 'держи')
+        elif 'красив' in upd:
+            user_id = update[0][3]
+            group_id = -64241379
+            post_id = get_last_post(group_id, 1, 1, 'owner')
+            attach = 'wall' + str(group_id) + '_' + str(post_id)
+            print(attach)
+            write_msg_attach(user_id, 'вот тебе красота', attach)
         else:
             # print(update)
             user_id = update[0][3]
