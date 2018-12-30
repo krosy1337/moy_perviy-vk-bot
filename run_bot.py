7139cf49e152ef36d6949749c68b3b9501c51440
krosy1337 <krosy1337@gmail.com>
2018-12-30 22:22:18 +0300

    supadupa

--- run_bot.py
+++ run_bot.py
@@ -1,20 +1,14 @@
 import requests
 from utility import *
 
-def masaga(slovko, user_id, photo, messag, user_idb , photob, messagb, user_idc, photoc, messagc):
+def masaga(slovko, gaga,gaga2,gaga3,user_id, photo, messag, user_idb , photob, messagb, user_idc, photoc, messagc):
     if slovo == slovko:
-        if upd == 'a':
-            vk_bot.method('messages.send',
-                            {'user_id': user_id, 'attachment': photo, 'message': messag,
-                            'random_id': random.randint(0, 1000)})
-        elif upd == 'b':
-                vk_bot.method('messages.send',
-                            {'user_id': user_idb, 'attachment': photob, 'message': messagb,
-                            'random_id': random.randint(0, 1000)})
-        elif upd == 'c':
-                vk_bot.method('messages.send',
-                            {'user_id': user_idc, 'attachment': photoc, 'message': messagc,
-                            'random_id': random.randint(0, 1000)})
+        if gaga == 'a':
+            send_photo(user_id, photo, messag )
+        elif gaga2 == 'b':
+            send_photo(user_idb, photob, messagb)
+        elif gaga3 == 'c':
+            send_photo(user_idc, photoc, messagc)
 
 def masags(hah, user_id, photos, messags,keyboard, user_idb, photosb, messagsb, keyboardb,user_idc,photosc, messagsc, keyboardc):
     if slovo2 == hah:
@@ -68,13 +62,13 @@ while True:
                     send_photo(user_id, 'photo-174113882_456239153', 'Ты в комнате C')
                 msg1 = update[0][1]
             elif ind == msg1 + 2:
-                masaga('a', user_id, 'photo-174113882_456239138', 'Ты в комнате AA',
+                masaga('a', upd, upd, upd, user_id, 'photo-174113882_456239138', 'Ты в комнате AA',
                        user_id, 'photo-174113882_456239139', 'Ты в комнате AB',
                        user_id, 'photo-174113882_456239147', 'Ты в комнате AC')
-                masaga('b', user_id, 'photo-174113882_456239150', 'Ты в комнате BA',
+                masaga('b', upd, upd, upd, user_id, 'photo-174113882_456239150', 'Ты в комнате BA',
                        user_id, 'photo-174113882_456239151', 'Ты в комнате BB',
                        user_id, 'photo-174113882_456239152', 'Ты в комнате BC')
-                masaga('c', user_id, 'photo-174113882_456239156', 'Ты в комнате CA',
+                masaga('c', upd, upd, upd, user_id, 'photo-174113882_456239156', 'Ты в комнате CA',
                        user_id, 'photo-174113882_456239155', 'Ты в комнате CB',
                        user_id, 'photo-174113882_456239154', 'Ты в комнате CC')
                 if upd == 'назад':
@@ -94,27 +88,27 @@ while True:
                         send_photo(user_id, 'photo-174113882_456239153', 'Ты в комнате C')
                     msg1 = ind
                 if slovo == 'a':
-                    masags('a', user_id, 'photo-174113882_456239157', 'Ты в комнате AAA', keyboard,
+                    masags('a', user_id, 'photo-174113882_456239157', 'Ты в комнате AAA',keyboard,
                            user_id, 'photo-174113882_456239159', 'Ты в комнате AAB', keyboard,
                            user_id, 'photo-174113882_456239158', 'Ты в комнате AAC', keyboard)
                     masags('b', user_id, 'photo-174113882_456239161', 'Ты в комнате ABA', keyboard,
                            user_id, 'photo-174113882_456239162', 'Ты в комнате ABB', keyboard,
                            user_id, 'photo-174113882_456239160', 'Ты в комнате ABC.Молодец, ты нашёл выход!!!', endkey)
                 elif slovo == 'b':
-                    masags('a',user_id, 'photo-174113882_456239163', 'Ты в комнате BAA', keyboard,
+                    masags('a', user_id, 'photo-174113882_456239163', 'Ты в комнате BAA', keyboard,
                            user_id, 'photo-174113882_456239162', 'Ты в комнате BAB', keyboard,
                            user_id, 'photo-174113882_456239161', 'Ты в комнате BAC', keyboard)
-                    masags('b',user_id, 'photo-174113882_456239158', 'Ты в комнате BBA', keyboard,
+                    masags('b', user_id, 'photo-174113882_456239158', 'Ты в комнате BBA', keyboard,
                            user_id, 'photo-174113882_456239159', 'Ты в комнате BBB', keyboard,
                            user_id, 'photo-174113882_456239161', 'Ты в комнате BBC', keyboard)
-                    masags('c',user_id, 'photo-174113882_456239159', 'Ты в комнате BCA', keyboard,
+                    masags('c', user_id, 'photo-174113882_456239159', 'Ты в комнате BCA', keyboard,
                            user_id, 'photo-174113882_456239164', 'Ты в комнате BCB', keyboard,
                            user_id, 'photo-174113882_456239161', 'Ты в комнате BCC', keyboard)
                 elif slovo == 'c':
-                    masags('a',user_id,'photo-174113882_456239158', 'Ты в комнате CAA', keyboard,
+                    masags('a', user_id,'photo-174113882_456239158', 'Ты в комнате CAA', keyboard,
                            user_id, 'photo-174113882_456239159', 'Ты в комнате CAB', keyboard,
                            user_id, 'photo-174113882_456239161', 'Ты в комнате CAC', keyboard)
-                    masags('b',user_id,'photo-174113882_456239161', 'Ты в комнате CBA', keyboard,
+                    masags('b', user_id, 'photo-174113882_456239161', 'Ты в комнате CBA', keyboard,
                            user_id, 'photo-174113882_456239162', 'Ты в комнате CBB', keyboard,
                            user_id, 'photo-174113882_456239158', 'Ты в комнате CBC', keyboard)
                 slovo3 = upd
@@ -129,19 +123,25 @@ while True:
                             if slovo3 == 'a' or slovo3 == 'b':
                                 send_photo(user_id, 'photo-174113882_456239139', 'Ты в комнате AB')
                                 if slovo3 == 'c':
-                                    send_photo(user_id,'','ТЫ вышел из одземелья, назад нельзя)')
-                    elif slovo == 'b':
-                        if slovo2 == 'a':
-                            send_photo(user_id, 'photo-174113882_456239150', 'Ты в комнате BA')
-                        elif slovo2 == 'b':
-                            send_photo(user_id, 'photo-174113882_456239151', 'Ты в комнате BB')
-                        elif slovo2 == 'c':
-                            send_photo(user_id, 'photo-174113882_456239152', 'Ты в комнате BC')
-                    elif slovo == 'c':
-                        if slovo2 == 'a':
-                            send_photo(user_id, 'photo-174113882_456239156', 'Ты в комнате CA')
-                        elif slovo2 == 'b':
-                            send_photo(user_id, 'photo-174113882_456239155', 'Ты в комнате CB')
+                                    send_photo(user_id, '', 'ТЫ вышел из одземелья, назад нельзя)')
+                    masaga('b', slovo2, slovo2, slovo2, user_id, 'photo-174113882_456239150', 'Ты в комнате BA',
+                           user_id, 'photo-174113882_456239151', 'Ты в комнате BB',
+                           user_id, 'photo-174113882_456239152', 'Ты в комнате BC')
+                    masaga('c', slovo2, slovo2, slovo2, user_id, 'photo-174113882_456239156', 'Ты в комнате CA',
+                           user_id, 'photo-174113882_456239155', 'Ты в комнате CB',
+                           user_id, '', '')
+                    # elif slovo == 'b':
+                    #     if slovo2 == 'a':
+                    #         send_photo(user_id, 'photo-174113882_456239150', 'Ты в комнате BA')
+                    #     elif slovo2 == 'b':
+                    #         send_photo(user_id, 'photo-174113882_456239151', 'Ты в комнате BB')
+                    #     elif slovo2 == 'c':
+                    #         send_photo(user_id, 'photo-174113882_456239152', 'Ты в комнате BC')
+                    # elif slovo == 'c':
+                    #     if slovo2 == 'a':
+                    #         send_photo(user_id, 'photo-174113882_456239156', 'Ты в комнате CA')
+                    #     elif slovo2 == 'b':
+                    #         send_photo(user_id, 'photo-174113882_456239155', 'Ты в комнате CB')
                     msg2 = ind
 
 
